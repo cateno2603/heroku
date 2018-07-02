b3d8a6075826af2bf2b7796f24651150f0be595f
cxvcx vcxvxcv <halexuan@hotmail.com>
2018-07-02 15:27:42 +0700

    ddd

---
+++ webcore1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace webcore1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
